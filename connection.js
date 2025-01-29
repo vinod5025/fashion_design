0e97d308c2540d890c84d9a740c6cde9503f3aba
vinod5025 <168267427+vinod5025@users.noreply.github.com>
2025-01-29 23:22:43 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ var util=require("util");
 var conn=mysql.createConnection({
     "host":"btaadmbkjzqdotolsxf4-mysql.services.clever-cloud.com",
     "user":"uop2n5ihjxah9mnj",
-    "password":"uop2n5ihjxah9mnj",
+    "password":"",
     "database":"btaadmbkjzqdotolsxf4"
 })
 var exe=util.promisify(conn.query).bind(conn);
