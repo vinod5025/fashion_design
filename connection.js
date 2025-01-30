72c548a370c318dc449cd5c2d891872b21ed7bfd
vinod5025 <168267427+vinod5025@users.noreply.github.com>
2025-01-30 21:57:24 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
 var mysql=require("mysql");
 var util=require("util");
 var conn=mysql.createConnection({
-    "host":"btaadmbkjzqdotolsxf4-mysql.services.clever-cloud.com",
-    "user":"uop2n5ihjxah9mnj",
-    "password":"",
-    "database":"btaadmbkjzqdotolsxf4"
+    "host":"bv26vzxlbhsdgcngogll-mysql.services.clever-cloud.com",
+    "user":"uzw7nxvwjugsqlap",
+    "password":"mQwUoMR8xvheL5F51W0u",
+    "database":"bv26vzxlbhsdgcngogll"
 })
 var exe=util.promisify(conn.query).bind(conn);
 module.exports=exe;
